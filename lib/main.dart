f4e91f8b8cf252568bf82db98ffeb1cc0e3895a3
Vivek Kekuda <vivekkekuda29@gmail.com>
2019-06-08 23:44:50 +0530

    fab action to change theme

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,40 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
 
-  ThemeData _themeData =ThemeData(
+  int _diceOne = 1;
+  int _diceTwo = 2;
+  bool _isDark = false;
+
+  ThemeData _themeDataLight =ThemeData(
     primarySwatch: Colors.deepOrange,
+    brightness: Brightness.light,
+    accentColor: Colors.deepOrangeAccent,
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      foregroundColor: Colors.white,
+    )
   );
+
+  ThemeData _themeDataDark =ThemeData(
+    primarySwatch: Colors.deepOrange,
+    brightness: Brightness.dark,
+    accentColor: Colors.deepOrangeAccent,
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      foregroundColor: Colors.black,
+    )
+  );
+
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dice Roll',
-      theme: _themeData,
+      theme: _isDark ? _themeDataDark : _themeDataLight,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Roll'),
@@ -31,12 +56,37 @@ class MyAppState extends State<MyApp> {
             ),
           ],
         ),
-        body: Container(),
+        body: Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Image.asset(
+                  'images/dice-${_isDark?'dark':'light'}-$_diceOne.png',
+                  color: Colors.deepOrange,
+                ),
+              ),
+              Expanded(
+                child: Image.asset(
+                  'images/dice-${_isDark?'dark':'light'}-$_diceTwo.png',
+                  color: Colors.deepOrange,
+                ),
+              ),
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton.extended(
           icon: Icon(Icons.casino),
           label: Text('Roll'),
           tooltip: 'Roll the dice',
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              if(_isDark)
+                _isDark = false;
+              else 
+                _isDark = true;
+              
+            });
+          },
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       )
